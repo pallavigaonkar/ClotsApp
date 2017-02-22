934d9cd1586156d6d36720261247d65dc713847e
sunil <sunilhiray1994@gmail.com>
2017-02-22 11:57:57 +0530

    change password

--- KotakApp/clots/src/main/java/myapp/winsofttech/com/kotak/LoginActivity.java
+++ KotakApp/clots/src/main/java/myapp/winsofttech/com/kotak/LoginActivity.java
@@ -32,7 +32,7 @@ public class LoginActivity extends AppCompatActivity {
         button_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(editText_password.getText().toString().equals("abcc")){
+                if(editText_password.getText().toString().equals("winsoft@123")){
                     Intent i = new Intent(getApplicationContext(),Home.class);
                     startActivity(i);
                 }else
